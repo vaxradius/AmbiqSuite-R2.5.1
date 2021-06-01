25aadddf80d954c8ee65074d0158d32d1a42a314
vaxradius <vaxradius@gmail.com>
2021-06-01 18:20:05 +0800

    Apollo1 Randomness

---
+++ boards/apollo1_evb/examples/Randomness/src/am_timebase.c
@@ -0,0 +1,74 @@
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include "am_mcu_apollo.h"
+
+//*****************************************************************************
+//
+// Global Variables
+//
+//*****************************************************************************
+volatile unsigned long long g_ui64SysTickWrappedTime = 0;
+
+#define AM_CORECLK_HZ     AM_HAL_CLKGEN_FREQ_MAX_HZ
+#define AM_CORECLK_KHZ    (AM_CORECLK_HZ/1000)
+#define AM_GET_MS_TICK    (((uint64_t)(0xFFFFFF - am_hal_systick_count()) + g_ui64SysTickWrappedTime)/AM_CORECLK_KHZ)
+
+//*****************************************************************************
+//
+// Systick ISR.
+//
+//*****************************************************************************
+void
+am_systick_isr(void)
+{
+    /* Add enough cycles to account for one full cycle of systick */
+    g_ui64SysTickWrappedTime += 0x01000000;
+}
+
+
+void am_timebase_busy_wait(uint32_t delay)
+{
+     am_util_delay_ms(delay);
+}
+
+volatile uint32_t g_ui32SysTickLast = 0xFFFFFF;
+volatile uint64_t g_ui64SysTickWrappedTimeLast = 0;
+
+// return ms 
+uint32_t am_timebase_get_tick(unsigned long long* u64tick)
+{
+	uint32_t critical;
+	uint32_t tickms;
+	unsigned long long tick;
+	uint32_t systick;
+	uint32_t CoreClk;
+
+	critical = am_hal_interrupt_master_disable();
+
+	systick = am_hal_systick_count();
+	if (g_ui64SysTickWrappedTime == g_ui64SysTickWrappedTimeLast && systick > g_ui32SysTickLast)
+		tick =(((0xFFFFFF - (unsigned long long)systick) + (0xFFFFFF - (unsigned long long)g_ui32SysTickLast) + g_ui64SysTickWrappedTime));
+	else
+		tick =(((0xFFFFFF - (unsigned long long)systick) + (unsigned long long)g_ui64SysTickWrappedTime));
+
+	*u64tick = tick;
+	tickms = tick/AM_CORECLK_KHZ;
+
+	g_ui32SysTickLast = systick;
+	g_ui64SysTickWrappedTimeLast = g_ui64SysTickWrappedTime;
+
+	am_hal_interrupt_master_set(critical);
+	return tickms;
+}
+
+void am_timebase_init(void)
+{
+    am_hal_systick_load(0x00FFFFFF);
+    am_hal_systick_int_enable();
+    am_hal_systick_start();
+}
---
+++ boards/apollo1_evb/examples/Randomness/src/randomness.c
@@ -0,0 +1,223 @@
+
+//*****************************************************************************
+//
+// Copyright (c) 2019, Ambiq Micro
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// Third party software included in this distribution is subject to the
+// additional license terms as defined in the /docs/licenses directory.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// This is part of revision v2.2.0-7-g63f7c2ba1 of the AmbiqSuite Development Package.
+//
+//*****************************************************************************
+
+#include "am_mcu_apollo.h"
+#include "am_bsp.h"
+#include "am_util.h"
+
+
+/*
+o	A) 100ms timer on LFRC. Measure # of HFRC cycles elapsed (~4,800,000). Repeat 100 times.
+o	B) 1s timer on LFRC. Measure # of HFRC cycle elapsed (~48,000,000). Repeat 100 times.
+o	C) 10s timer on LFRC. Measure # of HFRC cycles (~480,000,000). Repeat 100 times (17 minutes!).
+*/
+
+#define CTIMER_CLOCK_SOURCE AM_HAL_CTIMER_LFRC_512HZ
+#define MEASURE_PERIOD 512 //~100ms:51, 1s:512, 10s:5120  
+#define REPEAT_TIMES 20
+
+
+bool g_timer_flag = false;
+
+extern void am_timebase_init(void);
+extern uint32_t am_timebase_get_tick(unsigned long long* u64tick);
+
+
+//*****************************************************************************
+//
+// Timer configurations.
+//
+//*****************************************************************************
+am_hal_ctimer_config_t g_sTimer0 =
+{
+    // Don't link timers.
+    0,
+
+    // Set up TimerA0.
+    (AM_HAL_CTIMER_FN_REPEAT |
+     AM_HAL_CTIMER_INT_ENABLE |
+     CTIMER_CLOCK_SOURCE),
+
+    // No configuration required for TimerB0.
+    0,
+};
+
+
+//*****************************************************************************
+//
+// Init function for Timer A0.
+//
+//*****************************************************************************
+void
+timerA0_init(void)
+{
+	  //
+    // Enable the LFRC.
+    //
+    am_hal_clkgen_osc_start(AM_HAL_CLKGEN_OSC_LFRC);
+
+    //
+    // Set up timer A0.
+    //
+    am_hal_ctimer_clear(0, AM_HAL_CTIMER_TIMERA);
+    am_hal_ctimer_config(0, &g_sTimer0);
+
+    //
+    // Set the timing for timerA0.
+    //
+    am_hal_ctimer_period_set(0, AM_HAL_CTIMER_TIMERA, (MEASURE_PERIOD-1), 0);
+
+    //
+    // Clear the timer Interrupt
+    //
+    am_hal_ctimer_int_clear(AM_HAL_CTIMER_INT_TIMERA0);
+}
+
+//*****************************************************************************
+//
+// Timer Interrupt Service Routine (ISR)
+//
+//*****************************************************************************
+void
+am_ctimer_isr(void)
+{
+    //
+    // Clear TimerA0 Interrupt (write to clear).
+    //
+    am_hal_ctimer_int_clear(AM_HAL_CTIMER_INT_TIMERA0);
+	g_timer_flag = true;
+
+}
+
+//*****************************************************************************
+//
+// Main
+//
+//*****************************************************************************
+int
+main(void)
+{
+	unsigned long long u64tick_now = 0;
+	unsigned long long u64tick_last = 0;
+	int count = 0;
+	//
+    //
+    // Set the clock frequency.
+    //
+    am_hal_clkgen_sysclk_select(AM_HAL_CLKGEN_SYSCLK_MAX);
+
+    //
+    // Set the default cache configuration
+    //
+    am_hal_cachectrl_enable(&am_hal_cachectrl_defaults);
+
+
+    //
+    // Configure the board for low power operation.
+    //
+    //am_bsp_low_power_init();
+
+
+    //
+    // Initialize the printf interface for ITM/SWO output.
+    //
+    am_util_stdio_printf_init((am_util_stdio_print_char_t) am_bsp_itm_string_print);
+
+    //
+    // Initialize the SWO GPIO pin
+    //
+    am_bsp_pin_enable(ITM_SWO);
+
+    //
+    // Enable the ITM.
+    //
+    am_hal_itm_enable();
+
+    //
+    // Enable debug printf messages using ITM on SWO pin
+    //
+    am_bsp_debug_printf_enable();
+
+    //
+    // TimerA0 init.
+    //
+    timerA0_init();
+
+    am_timebase_init();
+
+    //
+    // Enable the timer Interrupt.
+    //
+    am_hal_ctimer_int_enable(AM_HAL_CTIMER_INT_TIMERA0);
+
+    //
+    // Enable the timer interrupt in the NVIC.
+    //
+    am_hal_interrupt_enable(AM_HAL_INTERRUPT_CTIMER);
+    am_hal_interrupt_master_enable();
+
+    //
+    // Enable the timer.
+    //
+    am_hal_ctimer_start(0, AM_HAL_CTIMER_TIMERA);
+
+	am_util_stdio_printf("Randomness\n\n");
+
+
+    //
+    // Loop forever, printing when awakened by the timer.
+    //
+    while (1)
+    {
+		//
+		// Go to Deep Sleep and wait for a wake up.
+		//
+		am_hal_sysctrl_sleep(AM_HAL_SYSCTRL_SLEEP_NORMAL);
+		if(g_timer_flag && count++ < REPEAT_TIMES)
+		{
+			g_timer_flag = false;
+			am_util_stdio_printf("%d.\t(%dms)\t",count,am_timebase_get_tick(&u64tick_now));
+			am_util_stdio_printf("%lld\n",u64tick_now-u64tick_last);
+			u64tick_last = u64tick_now;
+			if(count == REPEAT_TIMES)
+				am_util_stdio_printf("done\n\n");
+		}
+    }
+
+}
