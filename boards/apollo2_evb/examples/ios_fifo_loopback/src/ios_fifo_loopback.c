57995d029ba0f7ef01d15722ffa9579b900c7e0c
vaxradius <vaxradius@gmail.com>
2021-07-08 17:47:26 +0800

    Initiate ios_fifo_loopback

---
+++ boards/apollo2_evb/examples/ios_fifo_loopback/src/ios_fifo_loopback.c
@@ -0,0 +1,103 @@
+//*****************************************************************************
+//
+//! @file ios_fifo_loopback.c
+//!
+//! @brief Example slave used for demonstrating the use of the IOS FIFO.
+//!
+//! This slave component runs on one EVB and is used in conjunction with
+//! the companion host example, ios_fifo_host, which runs on a second EVB.
+//!
+//! The ios_fifo example has no print output.
+//! The host example does use the ITM SWO to let the user know progress and
+//! status of the demonstration.
+//!
+//! This example implements the slave part of a protocol for data exchange with
+//! an Apollo IO Master (IOM).  The host sends one byte commands on SPI/I2C by
+//! writing to offset 0x80.
+//!
+//! The command is issued by the host to Start/Stop Data accumulation, and also
+//! to acknowledge read-complete of a block of data.
+//!
+//! On the IOS side, once it is asked to start accumulating data (using START
+//! command), two CTimer based events emulate sensors sending data to IOS.
+//! When IOS has some data for host, it implements a state machine,
+//! synchronizing with the host.
+//!
+//! The IOS interrupts the host to indicate data availability. The host then
+//! reads the available data (as indicated by FIFOCTR) by READing using IOS FIFO
+//! (at address 0x7F).  The IOS keeps accumulating any new data coming in the
+//! background.
+//!
+//! Host sends an acknowledgement to IOS once it has finished reading a block
+//! of data initiated by IOS (partitally or complete). IOS interrupts the host
+//! again if and when it has more data for the host to read, and the cycle
+//! repeats - till host indicates that it is no longer interested in receiving
+//! data by sending STOP command.
+//!
+//! In order to run this example, a host device (e.g. a second EVB) must be set
+//! up to run the host example, ios_fifo_host.  The two boards can be connected
+//! using fly leads between the two boards as follows.
+//!
+//! @verbatim
+//! Pin connections for the I/O Master board to the I/O Slave board.
+//!
+//!     HOST (ios_fifo_host)                    SLAVE (ios_fifo)
+//!     --------------------                    ----------------
+//!     GPIO[10] GPIO Interrupt (slave to host) GPIO[4]  GPIO interrupt
+//!     GPIO[5]  IOM0 SPI CLK/I2C SCL           GPIO[0]  IOS SPI SCK/I2C SCL
+//!     GPIO[6]  IOM0 SPI MISO/I2C SDA          GPIO[1]  IOS SPI MISO/I2C SDA
+//!     GPIO[7]  IOM0 SPI MOSI                  GPIO[2]  IOS SPI MOSI
+//!     GPIO[11] IOM0 SPI nCE                   GPIO[3]  IOS SPI nCE
+//!     GND                                     GND
+//! @endverbatim
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Copyright (c) 2020, Ambiq Micro, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+// contributors may be used to endorse or promote products derived from this
+// software without specific prior written permission.
+//
+// Third party software included in this distribution is subject to the
+// additional license terms as defined in the /docs/licenses directory.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// This is part of revision 2.5.1 of the AmbiqSuite Development Package.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Main function.
+//
+//*****************************************************************************
+int
+main(void)
+{
+
+}
